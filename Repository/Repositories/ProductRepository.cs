68ed05f2c0de676cc060835161738a775293555a
bezginsasha <bezgin.sasha06@gmail.com>
2017-04-21 20:58:32 +0700

    fixProdRepoByKolya

---
+++ Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Repositories;
+using Repository.Interfaces;
+using Repository.Context;
+using Domain;
+
+namespace Repository.Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        ProductContext db;
+        public ProductRepository(ProductContext context) : base(context)
+        {
+            db = context;
+        }
+    }
+}
